a388e85cea096a0865754e60f1330e66bc07d256
davenarchives <daventheabyss@gmail.com>
2026-06-28 23:08:27 +0800

    chore: update src/components/layout/Navbar/index.tsx

--- src/components/layout/Navbar/index.tsx
+++
@@ -1,7 +0,0 @@
-import Navbar from './NavbarMain';
-export default Navbar;
-export { default as SearchBar } from './SearchBar';
-export { default as NavToggle } from './NavToggle';
-export { default as TitleLanguageToggle } from './TitleLanguageToggle';
-export { default as UserMenu } from './UserMenu';
-export { default as RandomButton } from './RandomButton';
