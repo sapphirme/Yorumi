760252e0d3e3c307bf3ea19c1a357cb2191b4159
davenarchives <daventheabyss@gmail.com>
2026-06-28 23:08:28 +0800

    chore: update src/features/anime/components/AnimeCardSkeleton.tsx

--- src/features/anime/components/AnimeCardSkeleton.tsx
+++ src/features/anime/components/AnimeCardSkeleton.tsx
@@ -7,25 +7,22 @@ interface AnimeCardSkeletonProps {
 
 const AnimeCardSkeleton: React.FC<AnimeCardSkeletonProps> = ({ className = '' }) => {
     return (
-        <div className={`relative flex flex-col gap-3 ${className}`}>
-            {/* Poster Skeleton */}
-            <div className="relative aspect-[2/3] w-full overflow-hidden rounded-xl">
-                <Skeleton className="w-full h-full" />
+        <div className={`relative z-0 ${className}`}>
+            {/* Poster Skeleton Wrapper */}
+            <div className="relative aspect-[2/3] rounded-lg overflow-hidden mb-3 shadow-lg ring-0 outline-none transition-all duration-75 ease-out">
+                <Skeleton className="w-full h-full absolute inset-0 rounded-none" />
                 
                 {/* Badges Skeletons */}
-                <div className="absolute top-2 right-2">
-                    <Skeleton className="w-10 h-5 rounded" />
-                </div>
-                <div className="absolute bottom-2 left-2 flex gap-1.5">
-                    <Skeleton className="w-8 h-5 rounded" />
-                    <Skeleton className="w-12 h-5 rounded" />
+                <div className="absolute bottom-2 left-2 flex gap-1.5 z-10">
+                    <Skeleton className="w-[45px] h-[24px] rounded" /> {/* Type badge approx width */}
+                    <Skeleton className="w-[38px] h-[24px] rounded" /> {/* Episode badge approx width */}
                 </div>
             </div>
             
             {/* Title Skeleton */}
-            <div className="space-y-2">
-                <Skeleton className="w-full h-4" />
-                <Skeleton className="w-2/3 h-4" />
+            <div className="space-y-1 mt-1">
+                <Skeleton className="w-[90%] h-4 rounded" />
+                <Skeleton className="w-[60%] h-4 rounded" />
             </div>
         </div>
     );
