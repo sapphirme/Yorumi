ccb4c6cae6d55139e52aba56ef87cb657c9bd3d1
davenarchives <daventheabyss@gmail.com>
2026-06-28 23:08:33 +0800

    chore: update src/types/index.ts

--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-// Re-export all types for convenience
-export type { Anime, Episode } from './anime';
-export type { Manga, MangaChapter, MangaPage } from './manga';
-export type { StreamLink } from './stream';
