27440b364d3832749af27b8634803ff6bea55e64
davenarchives <daventheabyss@gmail.com>
2026-06-28 23:08:24 +0800

    chore: update backend/src/infrastructure/cache/cache-store.ts

--- backend/src/infrastructure/cache/cache-store.ts
+++
@@ -1,8 +0,0 @@
-import { acquireLock, cacheGet, cacheSet, releaseLock } from '../../utils/redis-cache';
-
-export const cacheStore = {
-    get: cacheGet,
-    set: cacheSet,
-    acquireLock,
-    releaseLock,
-};
